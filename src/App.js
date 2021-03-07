09cf1ab68124242ec0431805d5d084933ad70602
Naim Morshed <morshed.naim13@gmail.com>
2021-03-07 11:54:07 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [familiar, setFamiliar] = useState(false);
+  const greetings = familiar ? 'Welcome, my friend.' : 'Who the hell are you?';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Is Familiar: {familiar.toString()}</h1>
+      <button onClick={() => setFamiliar(!familiar)}>Toggle</button>
+      <br /> <br/>
+      <h2>Greeting</h2>
+      <p>{greetings}</p>
+      <br/>
+      <h2>Food</h2>
+      {
+        familiar ? <p>I will buy food for you.</p> : <p>Lets eat, his his whose whose?</p>
+      }
+      <br/>
+      <h2>Communication</h2>
+      {
+        familiar ? <p>Lets join on facebook.</p> : <p>Sorry, I don't add strangers.</p>
+      }
     </div>
   );
 }
